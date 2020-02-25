58874852cbb44f9e507c4a214d65382d7f622fe0
Slogg <Dmitry.Ionas@gmail.com>
2020-02-26 01:06:44 +0300

    add Constant.cs

---
+++ GZipTest/Helper/Constant.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GZipTest.Helper
+{
+    internal static class Constant
+    {
+        public static readonly int ThreadsCount = Environment.ProcessorCount; 
+        public const int ChunkSize = 1048576;
+    }
+}
