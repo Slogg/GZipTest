8fe98cc7868db59d5c534ef60ea7e3643432f697
Slogg <Dmitry.Ionas@gmail.com>
2020-02-26 23:26:55 +0300

    add QueueSizeIdentifier.cs
    functionality for determining the optimal number of threads for a queue

---
+++ GZipTest/Domain/QueueSizeIdentifier.cs
@@ -0,0 +1,32 @@
+﻿using GZipTest.Helper;
+using Microsoft.VisualBasic.Devices;
+using System;
+
+namespace GZipTest.Domain
+{
+    internal sealed class QueueSizeIdentifier
+    {
+
+        /// <summary>
+        /// Получить максимальную размерность для очереди
+        /// </summary>
+        public int GetMaxSize()
+        {
+            var availablePhys = AvailablePhysical();
+            var querySize = (int)(availablePhys / 2 / Config.ChunkSize);
+            return Config.QueueSizeUpperBound > querySize ? querySize : Config.QueueSizeUpperBound;
+        }
+
+        // Доступное кол-во памяти, которое можно выделить для данных
+        private ulong AvailablePhysical()
+        {
+            var availableMem = new ComputerInfo().AvailablePhysicalMemory;
+            // Ограничить под 32 битную систему, если памяти доступно больше
+            if (!Environment.Is64BitProcess)
+            {
+                availableMem = availableMem > Config.AvailableBit32 ? Config.AvailableBit32 : availableMem;
+            }
+            return availableMem;
+        }
+    }
+}
